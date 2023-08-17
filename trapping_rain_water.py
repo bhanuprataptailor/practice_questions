7d762c2d529e98022160da7c047eb33e4549a088
bhanuprataptailor <bhanuprataptailor@gmail.com>
2023-08-17 13:59:23 +0530

    visualization added

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -1,7 +1,5 @@
-# Problem Statement: Consider an array of towers with width 1 unit, Calculate the amount of water trapped. HINT: plot a bar
-# chart of the given array and see the water trapped in between the towers.
-
-
+# Problem Statement: Consider an array of towers with width 1 unit, Calculate the amount of water trapped. HINT: plot
+# a bar chart of the given array and see the water trapped in between the towers.
 def calculate_area(towers, left, right):
     area = 0
     c = left + 1
@@ -31,5 +29,16 @@ def find_trapping_rain_water(towers):
     return total_water
 
 
-towers = [0, 2, 0, 3, 1, 4, 0, 5]
+def visualize_array(towers):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(15, 5))
+    plt.bar(range(0, len(towers)), towers, color='maroon', width=1)
+    plt.xlabel("Towers")
+    plt.ylabel("Height of towers")
+    plt.title("Visualization of towers")
+    plt.show()
+
+
+towers = [0, 1, 0, 2, 1, 0, 3, 1, 4, 5, 1, 5, 2, 6, 3, 6, 2, 7]
 print(find_trapping_rain_water(towers))
+visualize_array(towers)  # <- Use this to visualize your solutions
